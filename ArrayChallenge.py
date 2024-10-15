37246f3bd86c9766a8850d888e9d34eb03872248
Ashrafakram <126499311+Ashrafakram@users.noreply.github.com>
2024-10-15 18:38:46 +0530

    ArrayChallenge.py

--- ArrayChallenge.py
+++ ArrayChallenge.py
@@ -1,18 +1,19 @@
-children = {}
-parents = set()
-all_nodes = set()
-for pair in strArr:
-    child, parent = map(int, pair.strip('()').split(','))
-    all_nodes.add(child)
-    all_nodes.add(parent)
-if parent not in children:
-    children[parent] = []
-if len(children[parent]) >= 2:
-    return "false"
-children[parent].append(child)
-parents.add(child)
-root_candidates = all_nodes - parents
-if len(root_candidates) != 1:
-    return "false"
-return "true"
+def ArrayChalenge(strArr):
+    children = {}
+    parents = set()
+    all_nodes = set()
+    for pair in strArr:
+        child, parent = map(int, pair.strip('()').split(','))
+        all_nodes.add(child)
+        all_nodes.add(parent)
+    if parent not in children:
+        children[parent] = []
+    if len(children[parent]) >= 2:
+        return "false"
+    children[parent].append(child)
+    parents.add(child)
+    root_candidates = all_nodes - parents
+    if len(root_candidates) != 1:
+        return "false"
+    return "true"
 print(ArrayChallenge(input()))
